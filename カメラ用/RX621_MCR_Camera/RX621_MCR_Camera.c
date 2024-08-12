0280a527cae33d96b4cb56f41f5584e82574872c
hirata0517masato <32904561+hirata0517masato@users.noreply.github.com>
2024-08-12 17:27:28 +0900

    update
    ２値化の閾値を変更（元に戻す）

--- カメラ用/RX621_MCR_Camera/RX621_MCR_Camera.c
+++ カメラ用/RX621_MCR_Camera/RX621_MCR_Camera.c
@@ -520,11 +520,11 @@ void binarization(int linestart, int linestop)
 	//if(mode == 1) Ave = Min + 130;
 	//else Ave = ((Max + Min) >> 1) - 50;
 	//else{
-		/*a  = ((Max + Min) >> 1);
+		a  = ((Max + Min) >> 1);
 		Ave = ((a+Min) >> 1);
 		Ave = ((a+Ave) >> 1);
-		*/
-		Ave  = ((Max + Min) >> 1);
+		
+		//Ave  = ((Max + Min) >> 1);
 	//}
 	
 	/* •‚Í‚O@”’‚Í‚P‚É‚·‚é */
